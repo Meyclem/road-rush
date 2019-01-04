b487edf3425c01986fa42fac60641da4258de683
Clement Meyer <meyclem@gmail.com>
2019-01-04 14:17:20 +0100

    adds obstacles

--- src/assets/js/game/classes/road.js
+++ src/assets/js/game/classes/road.js
@@ -4,17 +4,34 @@ export class Road extends Phaser.GameObjects.Container {
   constructor(config) {
     super(config.scene)
     this.scene = config.scene
-    const game = this.scene.game
+    this.game = this.scene.game
 
     this.back = this.scene.add.image(0, 0, 'road')
     this.add(this.back)
     this.scene.add.existing(this)
     
-    Align.scaleToGameW(this.back, game, 0.5)
-    this.setSize(this.back.displayWidth, game.config.height)
+    Align.scaleToGameW(this.back, this.game, 0.5)
+    this.setSize(this.back.displayWidth, this.game.config.height)
 
     this.lineGroup = this.scene.add.group()
     this.count = 0
+
+    this.car = this.scene.add.sprite(this.displayWidth / 4, this.game.config.height * 0.9, "cars")
+    Align.scaleToGameW(this.car, this.game, 0.10)
+    this.add(this.car)
+
+    this.back.setInteractive()
+    this.back.on('pointerdown', this.changeLanes, this)
+
+    this.addObject()
+  }
+
+  changeLanes () {
+    if (this.car.x > 0) {
+      this.car.x = - this.displayWidth / 4
+    } else {
+      this.car.x = this.displayWidth / 4
+    }
   }
 
   makeLines () {
@@ -37,6 +54,46 @@ export class Road extends Phaser.GameObjects.Container {
       this.lineGroup.children.iterate(function(child) {
         child.y = child.oy
       }.bind(this))
+    }
+  }
+
+  addObject () {
+    const objs = [
+      {
+        key: 'pcar1',
+        speed: 10,
+        scale: 0.10
+      },
+      {
+        key: 'pcar2',
+        speed: 12,
+        scale: 0.10
+      },
+      {
+        key: 'cone',
+        speed: 20,
+        scale: 0.05
+      },
+      {
+        key: 'barrier',
+        speed: 20,
+        scale: 0.08
+      },
+    ]
+    const obj = objs[Math.floor(Math.random() * objs.length)]
+    this.object = this.scene.add.sprite(- this.displayWidth / 4, -100, obj.key)
+    this.object.speed = obj.speed
+    const lane = Math.random() * 100
+    if (lane > 50) { this.object.x = this.displayWidth / 4}
+    Align.scaleToGameW(this.object, this.game, obj.scale)
+    this.add(this.object)
+  }
+
+  moveObject () {
+    this.object.y += this.vSpace / this.object.speed
+    if (this.object.y > this.game.config.height + 100) {
+      this.object.destroy()
+      this.addObject()
     }
   }
 }
--- src/assets/js/game/main.js
+++ src/assets/js/game/main.js
@@ -10,6 +10,10 @@ export class SceneMain extends Phaser.Scene {
     // this.load.image('logo', `${gamePath}logo.png`)
     this.load.image('road', `${gamePath}road.jpg`)
     this.load.image('line', `${gamePath}line.png`)
+    this.load.image('cone', `${gamePath}cone.png`)
+    this.load.image('pcar1', `${gamePath}pcar1.png`)
+    this.load.image('pcar2', `${gamePath}pcar2.png`)
+    this.load.image('barrier', `${gamePath}barrier.png`)
     this.load.spritesheet(
       'cars',
       `${gamePath}cars.png`,
@@ -25,5 +29,6 @@ export class SceneMain extends Phaser.Scene {
 
   update () {
     this.road.moveLines()
+    this.road.moveObject()
   }
 }
